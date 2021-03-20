713b9abeda136befc2b11f113770763ceb462e0b
Paulo <paulohsilva2001@gmail.com>
2021-03-20 18:15:41 -0300

    CRUD Scaffolding

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -31,10 +31,6 @@ namespace SalesWebMVC.Controllers
             return View();
         }
 
-        public IActionResult Departments()
-        {
-            return View();
-        }
 
         public IActionResult Privacy()
         {
