3e532f2edeb0c9ed2a98a8dd4c829bc9abbc78c2
Paulo <paulohsilva2001@gmail.com>
2021-03-29 17:14:28 -0300

    Foreign key not null (referential integrity)

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -15,6 +15,8 @@ namespace SalesWebMVC.Models
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
+
 
         public ICollection<SalesRecord> SalesRecords { get; set; } = new List<SalesRecord>();
 
